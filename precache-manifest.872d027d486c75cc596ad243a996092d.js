1ab5ce308e8bb78c8941f7cbd31bbbbd0fdab064
Karthick S <selvendrankarthick@gmail.com>
2020-02-17 23:44:12 +0530

    Updates

---
+++ precache-manifest.872d027d486c75cc596ad243a996092d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dbfac10290666ab429dd4abf2e3c3d",
+    "url": "/compass_clock/index.html"
+  },
+  {
+    "revision": "820295ed870f155d7f74",
+    "url": "/compass_clock/static/css/main.d0d0f722.chunk.css"
+  },
+  {
+    "revision": "49f5b028d0adde876a14",
+    "url": "/compass_clock/static/js/2.85f241f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/compass_clock/static/js/2.85f241f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820295ed870f155d7f74",
+    "url": "/compass_clock/static/js/main.c767b154.chunk.js"
+  },
+  {
+    "revision": "4ceb0f25eaec45483f25",
+    "url": "/compass_clock/static/js/runtime-main.081cbc80.js"
+  }
+]);
